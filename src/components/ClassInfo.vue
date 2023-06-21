d309d8a630233089e6be9261fa1f36d4a7bfb785
haipa-nishi <108124535+haipa-nishi@users.noreply.github.com>
2023-06-21 21:04:58 +0900

    暫定こんな感じで

--- src/components/ClassInfo.vue
+++ src/components/ClassInfo.vue
@@ -2,15 +2,30 @@
 <template>
   <div >
     <h1>授業情報</h1>
-    <h2>{{ classData.subject }}</h2>
-    <h2>教員：{{ classData.teacher }}</h2>
-    <h3>学期： {{ classData.semester }}</h3>
+    <h2>科目名：{{ classData.subject }}</h2>
+    <h3 v-if="classData.required">【必修科目】</h3>
+    <h3 v-else>【選択科目】</h3>
+    <h2>担当教員：{{ classData.teacher }}</h2>
+    <h3>学期：{{ classData.semester }}</h3>
+    
+
     <div>
-      <h3>履修可能学年</h3>
-      <p>1年: {{ classData['1st year'] ? '可' : '不可' }}</p>
-      <p>2年: {{ classData['2nd year'] ? '可' : '不可' }}</p>
-      <p>3年: {{ classData['3rd year'] ? '可' : '不可' }}</p>
-      <p>4年: {{ classData['4th year'] ? '可' : '不可' }}</p>
+      <h3>配当年次</h3>
+      <p v-if="classData['1st year'] && classData.required">1年次：必修</p>
+      <p v-else-if="classData['1st year']">1年次：可</p>
+      <p v-else>1年次：不可</p>
+
+      <p v-if="classData['2nd year'] && classData.required">2年次：必修</p>
+      <p v-else-if="classData['2nd year']">2年次：可</p>
+      <p v-else>2年次：不可</p>
+
+      <p v-if="classData['3rd year'] && classData.required">3年次：必修</p>
+      <p v-else-if="classData['3rd year']">3年次：可</p>
+      <p v-else>3年次：不可</p>
+
+      <p v-if="classData['4th year'] && classData.required">4年次：必修</p>
+      <p v-else-if="classData['4th year']">4年次:可</p>
+      <p v-else>4年次：不可</p>
     </div>
     <div>
       <h3>授業実施日時</h3>
